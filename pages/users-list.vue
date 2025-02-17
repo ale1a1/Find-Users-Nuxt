c1f80dedbc2f35edb5a5c377a13ce6e616d979c2
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-02-17 12:42:40 +0000

    minor style fixes

--- pages/users-list.vue
+++ pages/users-list.vue
@@ -3,11 +3,11 @@
 
 <template>
   <div class="flex flex-col w-full">
-    <div class="flex justify-center mt-[10vh]">
-      <h1 class="text-red-900 text-3xl">USER LIST</h1>
+    <div class="flex justify-center mt-[8vh]">
+      <h1 class="text-red-900 text-4xl">USER LIST</h1>
     </div>
     <div class="flex-grow flex items-center justify-center">
-      <div class="w-[70vw] h-[40vh] border-4 border-red-500 flex items-center justify-center">
+      <div class="w-[75vw] h-[50vh] border-4 border-red-500 flex items-center justify-center">
         <p class="text-2xl">some content...</p>
       </div>
     </div>
