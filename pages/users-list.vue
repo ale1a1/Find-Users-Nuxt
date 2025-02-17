26614e05ead915f5c2f492cbac74a76d1ec817f4
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-02-17 12:33:17 +0000

    add template to login page and basic template to all pages

--- pages/users-list.vue
+++ pages/users-list.vue
@@ -2,5 +2,14 @@
 </script>
 
 <template>
-  <h1 class="text-red-900 text-2xl">USERS LIST</h1>
+  <div class="flex flex-col w-full">
+    <div class="flex justify-center mt-[10vh]">
+      <h1 class="text-red-900 text-3xl">USER LIST</h1>
+    </div>
+    <div class="flex-grow flex items-center justify-center">
+      <div class="w-[70vw] h-[40vh] border-4 border-red-500 flex items-center justify-center">
+        <p class="text-2xl">some content...</p>
+      </div>
+    </div>
+  </div>
 </template>
