241bfb2d0ff2e319a1dca15b2026bbef07a06500
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-03-22 23:56:57 +0000

    sort mobile navbar- minor bug...

--- app.vue
+++ app.vue
@@ -179,13 +179,13 @@ watchEffect(() => {
           <div class="relative flex h-12 items-center justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
               <!-- Mobile menu button-->
-              <button @click.stop="toggleMobileNavMenu" type="button" class="cursor-pointer relative inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
+              <button @click="toggleMobileNavMenu" type="button" class="cursor-pointer relative inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
                 <span class="absolute -inset-0.5"></span>
                 <span class="sr-only">Open main menu</span>      
-                <svg class="block size-7" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                <svg v-if="!isMobileNavMenuOpen" class="block size-7" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>         
-                <svg class="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                <svg v-else class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
               </button>
@@ -234,7 +234,7 @@ watchEffect(() => {
           </div>
         </div>
         <!-- Mobile menu, show/hide based on menu state. -->
-        <div v-click-outside="toggleMobileNavMenu" v-if="isMobileNavMenuOpen" class="sm:hidden" id="mobile-menu">
+        <div v-click-outside="toggleMobileNavMenu" v-if="isMobileNavMenuOpen" class="sm:hidden" id="mobile-menu" @click.stop>
           <div class="space-y-1 px-2 pt-2 pb-3">
             <NuxtLink @click="toggleMobileNavMenu" to="/" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300" aria-current="page">Home</NuxtLink>
             <NuxtLink @click="toggleMobileNavMenu" to="/users-list" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Users list</NuxtLink>
