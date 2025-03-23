9edf3778ea6f6be2f9aa6346a2931f1df2f21f38
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-03-23 00:18:14 +0000

    sort mobile navbar- minor bug.........

--- app.vue
+++ app.vue
@@ -180,7 +180,7 @@ watchEffect(() => {
           <div class="relative flex h-12 items-center justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
               <!-- Mobile menu button-->
-              <button @click="toggleMobileNavMenu" type="button" class="cursor-pointer relative inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
+              <div @click="!isMobileNavMenuOpen ? isMobileNavMenuOpen = true : null" type="button" class="cursor-pointer relative inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
                 <span class="absolute -inset-0.5"></span>
                 <span class="sr-only">Open main menu</span>      
                 <svg v-if="!isMobileNavMenuOpen" class="block size-7" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
@@ -189,7 +189,7 @@ watchEffect(() => {
                 <svg v-else class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
-              </button>
+              </div>
             </div>
             <div class="flex flex-1 gap-24 items-center justify-center sm:items-stretch sm:justify-start">
               <div class="flex shrink-0 items-center cursor-default">
