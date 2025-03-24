1382fdd55dd38fc7b6e29e2ad29ad2e29fd824d2
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-03-24 14:46:51 +0000

    create 404

---
+++ error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-neutral-900 text-white">
+    <div class="text-center max-w-xl p-8 bg-neutral-900/70 rounded-lg shadow-xl">
+      <h1 class="text-6xl font-bold text-amber-400">404</h1>
+      <p class="mt-4 text-xl text-gray-200">Oops! The page you are looking for doesn't exist.</p>
+      <div class="mt-6">
+        <NuxtLink to="/" class="text-amber-400 hover:underline text-lg font-semibold">
+          Go back to the homepage
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
