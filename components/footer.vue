44f6c25d8f557a8f28a1a1864db428be12d92269
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2025-03-29 11:24:19 +0000

    nav bar icon...

--- components/footer.vue
+++ components/footer.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { Instagram, Linkedin, Github, Youtube, Twitter, Twitch } from "lucide-vue-next";
+import { Instagram, Linkedin, Github, Youtube, Twitch } from "lucide-vue-next";
 
 const socialLinks = ref([
   { icon: Instagram, link: "https://instagram.com" },
-  { icon: Linkedin, link: "https://linkedin.com" },
-  { icon: Github, link: "https://github.com" },
+  { icon: Linkedin, link: "https://linkedin.com/in/alessandro-ladu-69196a239" },
+  { icon: Github, link: "https://github.com/ale1a1" },
   { icon: Youtube, link: "https://youtube.com" },
-  { icon: Twitter, link: "https://twitter.com" },
   { icon: Twitch, link: "https://twitch.tv" },
 ]);
 </script>
 
 <template>
   <footer class="border-t-2 border-amber-400/40 py-4 bg-neutral-950 text-gray-400 w-full">
-    <div class="w-full flex flex-col sm:flex-row justify-between items-center px-2 sm:px-6 lg:px-8 space-y-6 sm:space-y-0">
+    <div class="w-full flex flex-col sm:flex-row justify-between items-center px-2 sm:px-6 lg:ps-8 lg:pe-9 space-y-6 sm:space-y-0">
       
       <!-- Left Side: Copyright -->
       <p class="sm:text-sm cursor-default">© {{ new Date().getFullYear() }} ALW Media</p>
